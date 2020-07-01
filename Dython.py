d290f3cf9946f21b7f7a0575e69d7e5555a50119
Daniel <55925854+Sombrero64@users.noreply.github.com>
2020-07-01 15:57:21 -0500

    Update Dython.py

--- Dython.py
+++ Dython.py
@@ -10,6 +10,12 @@ def itemsMulty(var):
     for i in list(var):
         num *= float(i)
     return num
+
+def itemCount(var, item):
+    count = 0
+    for i in list(var):
+        if i is item: count += 1
+    return int(count)
     
 def rangeLimit(num, MIN, MAX):
     if int(num) > int(MAX): return int(MAX)
@@ -98,3 +104,87 @@ def menu(caption, options, noAnwser):
             if int(optionAnswer) >= 0 and int(optionAnswer) < int(len(options)): return int(optionAnswer)
         elif mest == "str":
             if bool(noAnwser) and str(optionAnwser) == "": return None
+
+# Objects
+# object: [subject, class, children, props]
+# prop: [name, value, default]
+
+def prop(name, value): return [str(name), value, value]
+
+def dump(Instance):
+    try: return [Instance.SUB, Instance.CLASS, Instance.PROPS, Instance.CHILDS]
+    except: return [Instance[0].SUB, Instance[0].CLASS, Instance[0].PROPS, Instance[0].CHILDS]
+
+class Instance():
+    def __init__(self, Subject, Class, Props):
+        self.SUB = str(Subject)
+        self.CLASS = str(Class)
+        self.PROPS = list(Props)
+        self.CHILDS = []
+
+    # Info
+    def gitSub(self): return self.SUB
+    def gitClass(self): return self.CLASS
+    def gitProps(self): return self.PROPS
+
+    def sitSub(self, New): self.SUB = str(New)
+    def sitClass(self, New): self.CLASS = str(New)
+    def sitProps(self, New): self.PROPS = str(New)
+
+    def findProp(self, Prop):
+        for p in list(self.PROPS):
+            if p[0] is str(Prop): return p
+        return None
+    
+    def gitProp(self, Prop):
+        for p in list(self.PROPS):
+            if p[0] is str(Prop): return p[1]
+        return None
+
+    def sitProp(self, Prop, New):
+        for p in list(self.PROPS):
+            if p[0] is str(Prop):
+                p[1] = New
+                break
+
+        
+    # Objects
+    def newChild(self, Instance): self.CHILDS.append(Instance)
+
+    def clearChild(self, Child):
+        for oj in range(int(len(list(self.CHILDS)))):
+            if self.CHILDS[oj] is Child:
+                self.CHILDS.pop(oj)
+            break
+
+    def clearAllChildren(self): self.CHILDS = []
+
+    def replaceChild(self, Child, NewChild):
+        for oj in range(int(len(list(self.CHILDS)))):
+            if self.CHILDS[oj] is Child:
+                self.CHILDS[oj] = NewChild
+                break
+
+    # Find Objects
+    def children(self): return list(self.CHILDS)
+
+    def findChild(self, ChildName):
+        for o in list(self.CHILDS):
+            if dump(o)[0] is str(ChildName): return o
+        return None
+
+    def findFirstChild(self, ChildClass):
+        for o in list(self.CHILDS):
+            if dump(o)[1] is str(ChildClass): return o
+        return None
+
+    def locateForChild(self, ChildName, ChildClass):
+        for o in list(self.CHILDS):
+            if dump(o)[0] is str(ChildName) and dump(o)[1] is str(ChildClass): return o
+        return None
+
+    def findFirstChildren(self, ChildrenClass):
+        op = []
+        for o in list(self.CHILDS):
+            if dump(o)[1] is str(ChildClass): op.append(o)
+        return list(op)
